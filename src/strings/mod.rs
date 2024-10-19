0f88b93684ee859049225a307af2f79b24e39592
v.bessonov <bessonov@action-media.ru>
2024-10-19 20:35:51 +0300

    string objects learning

--- src/strings/mod.rs
+++ src/strings/mod.rs
@@ -1,6 +1,26 @@
 pub fn demo_string_handling(){
     println!("\nDemo string handling");
     using_string_literals();
+    using_string_objects();
+    using_mutable_string_objects();
+}
+
+fn using_mutable_string_objects() {
+    let mut s5 = String::from("   super");
+    s5.push_str("   bowl!   ");
+
+    let s6 = s5.trim().to_uppercase();
+
+    println!("s5: {}, ptr: {:p}, len: {}", s5, s5.as_ptr(), s5.len());
+    println!("s6: {}, ptr: {:p}, len: {}", s6, s6.as_ptr(), s6.len());
+}
+
+fn using_string_objects() {
+    let s3 = String::from("wales");
+    let s4: String = String::from("scotland");
+
+    println!("s3: {}, ptr: {:p}, len: {}", s3, s3.as_ptr(), s3.len());
+    println!("s4: {}, ptr: {:p}, len: {}", s4, s4.as_ptr(), s4.len());
 }
 
 fn using_string_literals() {
